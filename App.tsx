d3014f1a86b6c330a1401d8128066ad2e076065c
Erling Hauan <erling.hauan@digdir.no>
2024-04-28 16:35:25 +0200

    Begin work on creating new notes

--- App.tsx
+++ App.tsx
@@ -1,24 +1,49 @@
-import NoteTaker from "./components/NoteTaker";
-import WelcomeHeading from "./components/WelcomeHeading";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useEffect, useState } from "react";
+import NewNote from "./components/NewNote";
+import DisplayNote from "./components/DisplayNote";
+import { Text } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
+export interface Note {
+  id: string | number;
+  name: string;
+  content: string;
+}
+
 export default function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const fetchedNotes = [
+        { id: 1, name: "Note 1", content: "Content of Note 1" },
+        { id: 2, name: "Note 2", content: "Content of Note 2" },
+      ];
+      setNotes(fetchedNotes);
+    };
+
+    fetchNotes();
+  }, []);
   return (
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen
-          name="Home"
-          component={WelcomeHeading}
-          options={{ title: "Welcome" }}
+          name="Create new note"
+          component={NewNote}
+          initialParams={{ notes: notes }}
         />
-        <Drawer.Screen
-          name="NoteTaker"
-          component={NoteTaker}
-          options={{ title: "Note taker" }}
-        />
+        <Drawer.Screen name="DisplayNote" component={DisplayNote} />
+        {notes.map((note) => (
+          <Drawer.Screen
+            key={note.id}
+            name={"Note " + note.id}
+            component={DisplayNote}
+            initialParams={{ note: note }}
+          />
+        ))}
       </Drawer.Navigator>
     </NavigationContainer>
   );
---
+++ components/NewNote.tsx
@@ -0,0 +1,44 @@
+﻿import { Button, StyleSheet, Text, View } from "react-native";
+import { useId } from "react";
+import { Note } from "../App";
+
+interface NewNoteProps {
+  navigation: any;
+}
+
+const NewNote = ({ navigation }: NewNoteProps) => {
+  const newId = useId();
+
+  const handleNewNote = () => {
+    const newNote: Note = {
+      id: newId,
+      name: "Note" + newId,
+      content: "Content of the new note",
+    };
+
+    navigation.navigate("DisplayNote", {
+      note: newNote,
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Welcome to the note taking app!</Text>
+      <Button title="Create new note" onPress={handleNewNote} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  heading: {
+    marginBottom: "10%",
+  },
+});
+
+export default NewNote;

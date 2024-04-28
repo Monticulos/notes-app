ad16f8c22a222c014d7869b01f7884657e242dc4
Monticulos <erling.hauan@outlook.com>
2024-04-28 09:30:04 +0200

    Add NoteTaker component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NoteTaker from "./components/NoteTaker";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Write your note below!</Text>
+      <NoteTaker />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/NoteTaker.tsx
@@ -0,0 +1,31 @@
+﻿import React, {useState} from 'react';
+import {Text, TextInput, View} from 'react-native';
+
+const PizzaTranslator = () => {
+    const [text, setText] = useState('');
+    return (
+        <View style={{padding: 10}}>
+            <TextInput
+                style={styles.input}
+                placeholder="Type here!"
+                onChangeText={newText => setText(newText)}
+                defaultValue={text}
+            />
+            <Text style={styles.textOutput}>
+                {text}
+            </Text>
+        </View>
+    );
+};
+
+const styles = {
+    input: {
+        height: 40
+    },
+    textOutput: {
+        padding: 10,
+        fontSize: 42
+    }
+}
+
+export default PizzaTranslator;
